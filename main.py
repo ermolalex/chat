f042f5a8d33d7bfda72c5d12d96d2136c33dbaca
ermolalex <alex@kik-soft.ru>
2025-04-07 11:18:43 +0300

    add deployment

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ async def lifespan(app: FastAPI):
     # dp.include_router(admin_router)
     await start_bot()
     webhook_url = settings.get_webhook_url()
-    # await bot.set_webhook(url=webhook_url,
-    #                       allowed_updates=dp.resolve_used_update_types(),
-    #                       drop_pending_updates=True)
-    # logging.info(f"Webhook set to {webhook_url}")
+    await bot.set_webhook(url=webhook_url,
+                          allowed_updates=dp.resolve_used_update_types(),
+                          drop_pending_updates=True)
+    logging.info(f"Webhook set to {webhook_url}")
     yield
     logging.info("Shutting down bot...")
     await bot.delete_webhook()
