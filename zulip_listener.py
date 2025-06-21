e95a2a1a1e051b85bec6c1ea17a0df23923c2212
ermolalex <alex@kik-soft.ru>
2025-06-22 00:23:13 +0300

    add sender fio

--- zulip_listener.py
+++ zulip_listener.py
@@ -32,14 +32,25 @@ def send_msg_to_bot(user_tg_id, text):
     print(results.json())
 
 
+def extract_tg_id_from_subject(subject: str):
+    try:
+        _, tg_id = tuple(subject.split("_"))
+        return tg_id
+    except ValueError:
+        msg_text = f"При попытке отправить сообщение из zulip, не удалось извлеч TG_ID из строки {subject}"
+        logger.error(msg_text)
+        send_msg_to_bot(settings.ADMIN_ID, msg_text)
+        return None
+
+
 def on_message(msg: dict):
     logger.info(msg)
     if msg["client"] == "website":
-        topic = msg["subject"]
-        phone, user_tg_id = tuple(topic.split("_"))
+        subject = msg["subject"]
+        user_tg_id = extract_tg_id_from_subject(subject)
         if user_tg_id and user_tg_id.isnumeric():
-            print(f"Сообщение в адрес {topic}: {msg['content']}")
-            send_msg_to_bot(user_tg_id, msg['content'])
+            msg_text = f"{msg['sender_full_name']}: msg['content']"
+            send_msg_to_bot(user_tg_id, msg_text)
 
 
 zulip_client.call_on_each_message(on_message)
@@ -54,9 +65,10 @@ zulip_client.call_on_each_message(on_message)
 # 'chat': {'id': 542393918, 'first_name': 'Александр', 'type': 'private'}, 'date': 1744282059, 'text': 'проблема 7'}}
 
 # от Zulip
-# {'id': 87, 'sender_id': 8, 'content': 'решение 6', 'recipient_id': 20, 'timestamp': 1744282105, 'client': 'website',
-# 'subject': 'от бота', 'topic_links': [], 'is_me_message': False, 'reactions': [], 'submessages': [], 'sender_full_name': 'Александр Ермолаев',
-# 'sender_email': 'alex@kik-soft.ru', 'sender_realm_str': '', 'display_recipient': '79219376763_542393918', 'type': 'stream', 'stream_id': 12,
-# 'avatar_url': None, 'content_type': 'text/x-markdown'}
+#{'id': 371, 'sender_id': 8, 'content': 'ping', 'recipient_id': 32, 'timestamp': 1750539450, 'client': 'website',
+# 'subject': 'Александр_542393918', 'topic_links': [], 'is_me_message': False, 'reactions': [], 'submessages': [],
+# 'sender_full_name': 'Александр Ермолаев', 'sender_email': 'alex@kik-soft.ru', 'sender_realm_str': '',
+# 'display_recipient': 'КиК-софт (тестовый)', 'type': 'stream', 'stream_id': 20, 'avatar_url': None, 'content_type': 'text/x-markdown'}
+#
 # {'ok': True, 'result': {'message_id': 481, 'from': {'id': 7586848030, 'is_bot': True, 'first_name': 'kik-test-bot', 'username': 'kik_soft_supp_bot'},
 # 'chat': {'id': 542393918, 'first_name': 'Александр', 'type': 'private'}, 'date': 1744282106, 'text': 'решение 6'}}
